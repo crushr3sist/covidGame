45fd544af7f638a141a6c1f01c17b21fb6b7b4fc
Ishchild ApartmentAngels <36351054+Wizock@users.noreply.github.com>
2022-04-18 01:29:29 +1000

    Update yourturn.py

--- yourturn.py
+++ yourturn.py
@@ -1,46 +1,48 @@
 from pygame_functions import *
 
-#imports sprite data
+# imports sprite data
 import spriteInit
 
-#loop intakes player movement variables and processes keypresses
+# loop intakes player movement variables and processes keypresses
 def movement(x, y, velocity, turnPassed, doctor):
     leftdoc = makeImage("doctor.png")
-    #allows intake from spriteInit variables
+    # allows intake from spriteInit variables
     driver = spriteInit.spriteRef()
-    #signifies beginning of while loop
-    done = 0  
+    # signifies beginning of while loop
+    done = 0
     while done == 0:
-        #if esc is pressed, the program ends
-        if keyPressed('esc'):
+        # if esc is pressed, the program ends
+        if keyPressed("esc"):
             end()
-        #if any of the following arrow keys are pressed, the character moves to the respected direction
-        if keyPressed('up'):
+        # if any of the following arrow keys are pressed, the character moves to the respected direction
+        if keyPressed("up"):
             y -= velocity
             done = 1
-        if keyPressed('down'):
+        if keyPressed("down"):
             y += velocity
             done = 1
-        if keyPressed('left'):
+        if keyPressed("left"):
             x -= velocity
             done = 1
-            
-        if keyPressed('right'):
+
+        if keyPressed("right"):
             x += velocity
             done = 1
-            
-    #tells the program that a turn has passed
+
+    # tells the program that a turn has passed
     turnPassed += 1
-    #returns the x and y variables back to the main loop to move the player
+    # returns the x and y variables back to the main loop to move the player
     return x, y, turnPassed, doctor
 
-#function for mask pickup
+
+# function for mask pickup
 def maskUsage(maskNum, score, doc, maskBox):
     if touching(doc, maskBox):
-        #adds 2 to the mask amount
+        # adds 2 to the mask amount
         maskNum += 2
-    #returns the mask amount to the main loop along with score
+    # returns the mask amount to the main loop along with score
     return maskNum, score
+
 
 def levelUp(level, score, oneTime2, oneTime3):
     if score <= 900:
@@ -59,4 +61,3 @@ def levelUp(level, score, oneTime2, oneTime3):
             oneTime3 = False
 
     return level, oneTime2, oneTime3
-
